25aec6bb8bb8a8ee9ce8a0b6072cd3e3b9b47e4b
Vannesha <vannesha.dev@gmail.com>
2022-02-16 10:32:31 +0000

    try to pull data from month 1

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -14,21 +15,24 @@ SHEET = GSPREAD_CLIENT.open('planning_doc')
 
 def get_month_data():
     """
-    Get month from the user
+    Get month from the user.
+    Run a while loop to collect the valid month from the user via the terminal, 
+    which must be from month 1 to 12. The loop will repeatedly request a valid 
+    data until the data collected is correct.
     """
-    print("Which month expenses are you looking for?")
-    print("Please state the month in terms of a number")
-    print("Example: If it is February, you should state 2\n")
+    while True:
+        print("Welcome to Wedding Budget Planner.\n")
+        print("Here is the your wedding planning budget at the moment.")
 
-    month = input("Enter the month here: ")
-
-    validate_data(month)
+        month = input("Enter the month here: ")
+        if validate_data(month):
+           print("Month has been noted, thank you.\n")
+           break
 
 def validate_data(values):
     """
-    Raises an error if the month selected is not a valid number.
+    Raises an error if the month selected is not a valid number or not an integer.
     """
-
     try:
         if int(values) >12:
             raise ValueError(
@@ -36,5 +40,23 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-get_month_data()
+    return True
+
+def show_expenses_month1(month):
+    if month(input) == "1":
+        print("Retrieving data")
+        expenses_data_m1 = SHEET.worksheet("expenses").cell(col=6 ,row=2)
+        print(expenses_data_m1)
+        
+    return month
+
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_month_data()
+    month = show_expenses_month1()
+main()
